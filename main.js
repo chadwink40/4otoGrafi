ca58c027619c7730dbdd93dd74009efb8e8bdc8e
Sodiq Oladeji <lolademyschool@gmail.com>
2022-08-22 18:35:40 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// Window Scroll Magic
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+})
+
+// Button P and rotating styles
+
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style = "transform: rotate(${index * 13}deg)">${character}</span>`).join('')
+})
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
+});
+
+// Hambugger and close btn menu
+const nav = document.querySelector('.nav_links');
+const closeNavBtn = document.querySelector('#nav__toggle-close')
+const openNavBtn = document.querySelector('#nav__toggle-open')
+
+
+const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+if (document.body.clientWidth < 1024) {
+    nav.querySelectorAll('li a').forEach(navLink => {
+        navLink.addEventListener('click', closeNav);
+    })
+}
